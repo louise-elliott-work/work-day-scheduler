7081ec4f1c226e28ac4b446580eaec9d24a3b3fa
Louise Elliott <louise.elliott.work@gmail.com>
2023-11-13 13:45:27 +0000

    current date display added - need to edit format

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 
 // * Current date and date in 'Monday, November 13th' format needs to display within HTML element: <p id="currentDay" class="lead"></p>
 
+var currentDay = $("#currentDay");
+
+// ! The intention here is to use advancedFormat to show the date as 1st, 2nd, 3rd, 4th, etc instead of only the number.
+var advancedFormat = require('dayjs/plugin/advancedFormat')
+dayjs.extend(advancedFormat)
+
+currentDay.text(dayjs().format('dddd, MMMM Do'));
+
+
+
 // * A table must display a day in one-hour time blocks, with the time written on the left, the time block with any text in the middle and a storage button on the right
 
 // * Text previously entered and stored locally must display within the time blocks
